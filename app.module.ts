43f77b839210a5bea80bb6ce42677ef9113b25b1
Imran Khan <imran.khan@kpn.com>
2018-12-12 15:39:44 +0100

    replaced deprecated HttpModule with HttpClientModule

--- app.module.ts
+++ app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ProductsListComponent } from './products-list/products-list.component';
@@ -22,7 +22,7 @@ import { ProductDepartmentComponent } from './product-department/product-departm
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent] // Top level component
